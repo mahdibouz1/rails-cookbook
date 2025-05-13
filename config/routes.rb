e8e00c33663c130d0df8d89c5497452b4facfa8a
Elmahdi Bouzgan <mahdibouz101@gmail.com>
2025-05-13 13:31:56 +0100

    navbar and banner complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
-
+  root "categories#index"
   resources :categories, except: [ :edit, :update, :destroy ] do
     resources :bookmarks, only: [ :new, :create ]
   end
