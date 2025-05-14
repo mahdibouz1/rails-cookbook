b86fb8fed05420ca24e8b67599096fe6b9b11f50
Elmahdi Bouzgan <mahdibouz101@gmail.com>
2025-05-14 17:17:08 +0100

    cookboook complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'json'
+require 'open-uri'
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -41,5 +43,30 @@ recipes = Recipe.create!([
     rating: 4.2
   }
 ])
+
+def recipe_builder(id)
+  url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=#{id}"
+  meal_serialized = URI.parse(url).read
+  meal = JSON.parse(meal_serialized)["meals"][0]
+
+
+  Recipe.create!(
+    name: meal["strMeal"],
+    description: meal["strInstructions"],
+    image_url: meal["strMealThumb"],
+    rating: rand(2..5.0).round(1),
+  )
+end
+
+categories = [ "Breakfast", "Pasta", "Seafood", "Dessert" ]
+
+categories.each do |category|
+  url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=#{category}"
+  recipe_list = URI.parse(url).read
+  recipes = JSON.parse(recipe_list)
+  recipes["meals"].take(5).each do |recipe|
+    recipe_builder(recipe["idMeal"])
+  end
+end
 
 puts "Created #{recipes.count} recipes!"
